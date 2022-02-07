9a8a7ce3cb7988c7a9fc64d0ae27860e382b2ff1
Daniel Chantres <daniel.chantres@live.u-tad.com>
2022-02-07 16:14:31 +0100

    cambio con metodos

--- BancoPY/Prueba.py
+++ BancoPY/Prueba.py
@@ -1,4 +1,48 @@
-hola me ves?
-
-si
- Daniel
+def alta():
+    print("Alta")
+def cambiarPin():
+    print("cambiar pin")
+def detalles():
+    print("mostrar detalles")
+def agregarSaldo():
+    print("Agregar saldo")
+def deposito():
+    print("deposito")
+def inversion():
+    print("inversion")
+def extraerDinero():
+    print("extraer dinero")
+def baja():
+    print("baja")
+opcion=(int)(input("""Seleciona una opcion:
+1.- Dar de alta.
+2.- Iniciar sesion"""))
+if opcion==1:
+    alta()
+elif opcion==2:
+    opcion = (int)(input("""Seleciona una opcion:
+    1.- Cambiar pin.
+    2.- Ver detalles.
+    3.- Añadir saldo.
+    4.- Hacer deposito
+    5.- Hacer inversion
+    6.- Sacar dinero
+    7.- Baja"""))
+    if opcion==1:
+        cambiarPin()
+    elif opcion==2:
+        detalles()
+    elif opcion==3:
+        agregarSaldo()
+    elif opcion==4:
+        deposito()
+    elif opcion==5:
+        inversion()
+    elif opcion==6:
+        extraerDinero()
+    elif opcion==7:
+        baja()
+    else:
+        print("Selecciona una opción valida")
+else:
+    print("Opcion no valida")
